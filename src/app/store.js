93fb5183a9ceddb61a9a0dff953ae45f5899ce9c
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-06-26 17:03:12 +0100

    posts slice added

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import postsReducers from '../features/posts/postsSlice';
 
 export default configureStore({
-  reducer: () => ({}),
+  reducer: {
+    reducer: postsReducers
+  },
 })
