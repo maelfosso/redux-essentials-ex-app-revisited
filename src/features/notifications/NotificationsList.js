dc363e7af114f0aa19c3209fcfcae31daeb2921d
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-06-28 13:11:42 +0100

    added new/read feature to notifications

--- src/features/notifications/NotificationsList.js
+++ src/features/notifications/NotificationsList.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { selectAllNotifications } from './notificationsSlice';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { allNotificationsRead, selectAllNotifications } from './notificationsSlice';
 import { selectAllUsers } from '../users/usersSlice';
 import { formatDistanceToNow, parseISO } from 'date-fns';
+import classNames from 'classnames';
 
 export const NotificationsList = () => {
+  const dispatch = useDispatch();
+
   const notifications = useSelector(selectAllNotifications);
   const users = useSelector(selectAllUsers);
+
+  useEffect(() => {
+    dispatch(allNotificationsRead());
+  });
 
   const renderedNotifications = notifications.map(notification => {
     const date = parseISO(notification.date);
@@ -15,8 +22,12 @@ export const NotificationsList = () => {
       name: "Unknown User"
     }
 
+    const notificationClassname = classNames('notification', {
+      new: notification.isNew
+    });
+
     return (
-      <div key={notification.id} className="notification">
+      <div key={notification.id} className={notificationClassname}>
         <div>
           <b>{ user.name }</b> { notification.message }
         </div>
--- src/features/notifications/notificationsSlice.js
+++ src/features/notifications/notificationsSlice.js
@@ -18,18 +18,25 @@ export const fetchNotifications = createAsyncThunk(
 const notificationsSlice = createSlice({
   name: 'notifications',
   initialState: [],
-  reducers: {},
+  reducers: {
+    allNotificationsRead(state, action) {
+      state.forEach(notification => {
+        notification.read = true;
+      });
+    }
+  },
   extraReducers: {
     [fetchNotifications.fulfilled]: (state, action) => {
-      console.log(action.payload);
+      state.forEach(notification => {
+        notification.isNew = !notification.read;
+      })
       state.push(...action.payload);
       state.sort((a, b) => b.date.localeCompare(a.date));
-    },
-    [fetchNotifications.rejected]: (state, action) => {
-      console.log(action);
     }
   }
 });
+
+export const { allNotificationsRead } = notificationsSlice.actions;
 
 export default notificationsSlice.reducer;
 
