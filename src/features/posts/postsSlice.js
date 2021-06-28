d90ab018fe968c32c03eb8f9973f73803385f8c3
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-06-28 09:56:04 +0100

    added a new action from thunk functions

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,12 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { sub } from 'date-fns';
 import { nanoid } from '@reduxjs/toolkit';
+import { client } from '../../api/client';
 
 const initialState = {
   posts: [],
   status: 'idle',
   error: null,
 }
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const response = await client.get('/fakeApi/posts');
+  return response.posts;
+});
 
 const postsSlice = createSlice({
   name: 'posts',
