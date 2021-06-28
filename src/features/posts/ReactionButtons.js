6eb5be5cd2b5755df5ab5fe54e250251ae2f3098
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-06-28 08:29:53 +0100

    update ReactionButtons component to connect it to redux-store

--- src/features/posts/ReactionButtons.js
+++ src/features/posts/ReactionButtons.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { reactionAdded } from './postsSlice';
 
 const reactionEmoji = {
   thumbsUp: '👍',
@@ -9,9 +11,16 @@ const reactionEmoji = {
 };
 
 export const ReactionButons = ({ post }) => {
+  const dispatch = useDispatch();
+
   const reactionsButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
     return (
-      <button key={name} type="button" className="muted-button reaction-button">
+      <button 
+        key={name} 
+        type="button" 
+        className="muted-button reaction-button"
+        onClick={() => dispatch(reactionAdded({ postId: post.id, reaction: name }))}
+      >
         {emoji} {post.reactions[name]}
       </button>
     );
