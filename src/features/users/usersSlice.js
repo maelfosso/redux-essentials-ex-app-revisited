29304bd1e6e9b7d872191c31897f980951efff1f
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-06-28 10:57:30 +0100

    added async request for adding post

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -14,7 +14,6 @@ const usersSlice = createSlice({
   reducers: {},
   extraReducers: {
     [fetchUsers.fulfilled]: (state, action) => {
-      console.log(state, action);
       return action.payload
     }
   }
